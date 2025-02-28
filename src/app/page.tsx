4aa3115af7bbc4ba23b971568e8b34090248252f
nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 00:14:18 +0100

    FEAT:: ui and layout of cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AddLetter from "@/components/AddLetter";
+import UseLetter from "@/components/UseLetter";
 
 export default function Home(){
   return(
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen p-5 lg:flex flex-row justify-center gap-3 items-center">
       <AddLetter />
+      <UseLetter />
     </div>
   )
 }
--- src/components/UseLetter.tsx
+++ src/components/UseLetter.tsx
@@ -1,9 +1,10 @@
-export default function AddLetter(){
+
+export default function UseLetter(){
     return(
         <div className="lg:w-1/3 w-full mb-3 h-64 grid-cols-1 items-start p-5 place-items-center grid rounded-md shadow-md border border-gray-200">
-            <h1 className="text-xl">Add new cover letter</h1>
+            <h1 className="text-xl">Use cover letter</h1>
             <button className="rounded-full bg-slate-50 cursor-pointer transition-all hover:bg-slate-100 w-32 h-32">
-            Add new letter to you base
+                Use existing letters
             </button>
       </div>
     )
