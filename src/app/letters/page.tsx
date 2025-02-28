d1304ae0077a30eb864ea013150e6008199a4908
nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 00:30:47 +0100

    FEAT:: setting links

---
+++ src/app/letters/page.tsx
@@ -0,0 +1,7 @@
+export default function lettersPage(){
+    return(
+        <div>
+            <h1>Letters page</h1>
+        </div>
+    )
+}
--- src/components/UseLetter.tsx
+++ src/components/UseLetter.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 
 export default function UseLetter(){
     return(
         <div className="lg:w-1/3 w-full mb-3 h-64 grid-cols-1 items-start p-5 place-items-center grid rounded-md shadow-md border border-gray-200">
             <h1 className="text-xl">Use cover letter</h1>
-            <button className="rounded-full bg-slate-50 cursor-pointer transition-all hover:bg-slate-100 w-32 h-32">
-                Use existing letters
-            </button>
+            <Link href={'/letters'}>
+                <button className="rounded-full bg-slate-50 cursor-pointer transition-all hover:bg-slate-100 w-32 h-32">
+                    Use existing letters
+                </button>
+            </Link>
       </div>
     )
 }
