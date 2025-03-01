ed39546b7745cc9b4bbfb6516d0b2044fd60947d
nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 01:10:36 +0100

    FIX:: setting env variables

--- src/app/newletter/page.tsx
+++ src/app/newletter/page.tsx
@@ -1,9 +1,29 @@
+"use client"
+import { db } from "@/utils/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react"
+
 export default function newLetter(){
+    const [letterData, setLetterData] = useState("");
+
+    const addLetter = async () => {
+        try{
+            const letterCollectionRef = collection(db, "letters");
+            await addDoc(letterCollectionRef, {
+                letter: letterData,
+            });
+        }
+        catch(err){
+            console.log(err)
+        }
+        alert("Letter submited")
+    }
+
     return(
         <div className="w-full p-5 h-screen grid grid-cols-1 justify-items-center">
-            <textarea className="lg:w-1/2 w-full lg:h-96 h-full border border-gray-300 px-5 shadow-md rounded-md" placeholder="Enter your generic cover letter here. . ."></textarea>
+            <textarea onChange={(e) => setLetterData(e.target.value)} className="lg:w-1/2 w-full lg:h-96 h-full border border-gray-300 px-5 shadow-md rounded-md" placeholder="Enter your generic cover letter here. . ."></textarea>
             <br />
-            <button className="bg-slate-900 rounded-md mt-3 shadow-md lg:w-1/2 w-full h-10 text-white cursor-pointer hover:bg-slate-800">Submit</button>
+            <button onClick={addLetter} className="bg-slate-900 rounded-md mt-3 shadow-md lg:w-1/2 w-full h-10 text-white cursor-pointer hover:bg-slate-800">Submit</button>
         </div>
     )
 }
