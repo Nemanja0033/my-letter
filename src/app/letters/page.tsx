632dac316efefdcce334c8f4fc82dddea83e1c65
nemanja <antonijevicnemanja68@gmail.com>
2025-03-01 14:57:31 +0100

    FEAT:: letters page

--- src/app/letters/page.tsx
+++ src/app/letters/page.tsx
@@ -1,7 +1,18 @@
-export default function lettersPage(){
+import { db } from "@/utils/firebase"
+import { collection, getDocs, query } from "firebase/firestore"
+
+async function getLetters() {
+    const q = query(collection(db, "letters"));
+    const data = await getDocs(q);
+    const letters = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return letters;
+}
+
+export default async function lettersPage(){
+    const letters = await getLetters();
     return(
-        <div>
-            <h1>Letters page</h1>
+        <div className="w-full grid lg:grid-cols-3 grid-cols-1 place-items-center">
+            
         </div>
     )
 }
